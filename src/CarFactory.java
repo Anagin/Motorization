3e88ef328de4e91a44f76d8db1d4502058aa71c3
Przemysław <przemekost@gmail.com>
2017-03-09 22:32:08 +0100

    New Array created. Cars stored in tab.

--- src/CarFactory.java
+++ src/CarFactory.java
@@ -5,12 +5,14 @@ public class CarFactory {
 
     public static void main(String[] args) {
         final String appName = "CarShop v0.1";
-        Car car1 = new Car("Audi", "A4", "B9", "sedan", 2017, "black");
-        Car car2 = new Car("Opel", "Astra", "H", "hatchback", 2011, "white");
+        Car[] cars = new Car[1000];
+        cars[0] = new Car("Audi", "A4", "B9", "sedan", 2017, "black");
+        cars[1] = new Car("Opel", "Astra", "H", "hatchback", 2011, "white");
 
         System.out.println("Application name: "+appName);
         System.out.println("Available cars:");
-        car1.printInfo();
-        car2.printInfo();
+        for(int i=0; i<cars.length; i++) {
+            cars[i].printInfo();
+        }
     }
 }
